7566169fade7aea2ef7382e9b0a6a0edd401300f
za <za@zhonganMacBook-Pro.local>
2018-08-09 18:30:03 +0800

    add reconice

--- one_data/controllers/reconice.py
+++ one_data/controllers/reconice.py
@@ -1,10 +1,21 @@
 # -*- coding:utf-8 -*-  
-# import tushare as ts
-# import pandas as pd
+import tushare as ts
+import pandas as pd
 from django.http import HttpResponse
 
 def reconice(request):
-    return HttpResponse("000001")
+
+    question=request.GET.get('question')
+    stock_basics = ts.get_stock_basics()
+    data = pd.DataFrame(stock_basics)
+
+    for index, row in data.iterrows():   # 获取每行的index、row
+        if row.name in question:
+            return HttpResponse("name" + row.name)
+        if row.code in question:
+            return HttpResponse("code:" + row.code)
+
+    return HttpResponse("我还不知道呢，你可以教我吗？“)
 
 
 
