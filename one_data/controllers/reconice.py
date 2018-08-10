dc9c72fb6683a12e21114f5f44e32461eb3a6093
za <za@zhonganMacBook-Pro.local>
2018-08-10 11:27:42 +0800

    delete test

--- one_data/controllers/reconice.py
+++ one_data/controllers/reconice.py
@@ -7,13 +7,15 @@ from django.http import HttpResponse
 def reconice(request):
     # request.encoding='utf-8'
     question=request.GET.get('question')
+    stock_name=request.GET.get('stock_name')
     # question=question.encode('utf-8')
     question=urllib.unquote(question)
+    stock_name=urllib.unquote(stock_name)
     stock_basics = ts.get_stock_basics()
     data = pd.DataFrame(stock_basics)
 
     for index, row in data.iterrows():   # 获取每行的index、row
-        if row["name"] in question:
+        if row["name"] == stock_name:
             return HttpResponse(row.name)
 
     return HttpResponse("我还不知道呢")
